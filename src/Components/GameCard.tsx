8a90e517dd1bc68e6b42b159eda2fb6fe6e1aa8e
AdamskiJakub <105269736+AdamskiJakub@users.noreply.github.com>
2023-08-21 21:31:49 +0200

    Adding emojis and required styles

--- src/Components/GameCard.tsx
+++ src/Components/GameCard.tsx
@@ -3,6 +3,7 @@ import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
 import PlatformIcon from "./PlatformIcon";
 import CriticScore from "./CriticScore";
 import noImage from "../assets/no-image-placeholder-6f3882e0.webp";
+import Emojis from "./Emojis";
 
 interface Props {
   game: Game;
@@ -13,13 +14,15 @@ const GameCard = ({ game }: Props) => {
     <Card borderRadius={10} overflow="hidden">
       <Image src={backgroundUrl} />
       <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
-        <HStack justifyContent="space-between">
+        <HStack justifyContent="space-between" marginBottom={3}>
           <PlatformIcon
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
           <CriticScore score={game.metacritic} />
         </HStack>
+        <Heading fontSize="2xl">
+          {game.name} <Emojis rating={game.rating_top} />
+        </Heading>
       </CardBody>
     </Card>
   );
